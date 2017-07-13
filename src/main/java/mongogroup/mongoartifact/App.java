21fe8fbbe5a33a8c8144932fe29d0d9712f4f894
Michael <Michael@Michael-PC>
2017-07-13 20:22:37 +0200

    test mongoDB

---
+++ src/main/java/mongogroup/mongoartifact/App.java
@@ -0,0 +1,44 @@
+package mongogroup.mongoartifact;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+		String connectPoint = mongo.getConnectPoint();
+		System.out.println(connectPoint);
+		
+		
+		MongoDatabase database = mongo.getDatabase("mycustomers");
+		MongoCollection collection = database.getCollection("customers");
+		
+		System.out.println(database.getName());
+		
+		List<Document> documents = (List<Document>) collection.find().into(
+				new ArrayList<Document>());
+ 
+		System.out.println("size="+documents.size());
+		
+               for(Document document : documents){
+                   System.out.println(document.get("first_name"));
+               }
+		
+		
+		mongo.close();
+    }
+}
